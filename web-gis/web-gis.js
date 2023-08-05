a912a9cfc8c2dbcf1fc6d087e541055801b6c2d4
Michaelojok11 <michaelocan11@gmail.com>
2023-08-06 01:36:20 +0300

    update git

---
+++ web-gis/web-gis.js
@@ -0,0 +1,25 @@
+// start with a variable
+/*
+let myLatitude = 0;
+if (myLatitude > 0){
+    alert("You are in the Northern hemisphere");    
+} else if (myLatitude < 0){
+    alert("You are in the Southern Hemisphere");
+} else {
+    alert("You are exactly on the equator");
+}
+*/
+
+// 
+
+// Exercise / Day 2 task
+    let myLatitude = 20;
+    if (myLatitude > 0){
+        alert("You are in the Western hemisphere");    
+    } else if (myLatitude < 0){
+        alert("You are in the Eastern Hemisphere");
+    } else {
+        alert("You are exactly on the equator");
+    }
+
+// 
